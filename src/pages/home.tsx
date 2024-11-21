2e53d3df5f727db6269da78edeff506013e73d24
Diogo L Scarmagnani <diogols@live.com>
2024-11-21 07:43:35 -0300

    form's

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,17 @@
+import { buttonVariants } from "@/components/ui/button";
 import { ROUTE_ENUM } from "@/types/route";
 import { Link } from "react-router-dom";
 
 export function Home() {
   return (
     <>
-      <p>Hello! This is my Home Page.</p>
-      <Link to={ROUTE_ENUM.LOGIN}>Login</Link>
+      <p className="mb-3">
+        Hello! This is your barber shop online.
+      </p>
+
+      <Link to={ROUTE_ENUM.LOGIN} className={buttonVariants({ variant: "secondary" })}>
+        Login
+      </Link>
     </>
   )
 }
