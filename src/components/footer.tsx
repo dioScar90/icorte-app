e897fe32580aed0378c2be2b975e0c4e0a4d5ac5
Diogo L Scarmagnani <diogols@live.com>
2024-12-01 14:29:16 -0300

    some changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,23 @@
+import { ROUTE_ENUM } from "@/types/route";
+import { Link } from "react-router-dom";
 
 export function Footer() {
   return (
     <footer>
       <div className="footer-container">
         <span className="text-center text-sm text-gray-500 dark:text-gray-400">
-          © 2024 <a href="https://flowbite.com/" className="hover:underline">iCorte™</a>. All Rights Reserved.
+          © 2024 <Link to={ROUTE_ENUM.ROOT} className="hover:underline">iCorte™</Link>. All Rights Reserved.
         </span>
 
         {/* <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Sobre</a>
+            <Link to="#" className="hover:underline me-4 md:me-6">Sobre</Link>
           </li>
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Política de privacidade</a>
+            <Link to="#" className="hover:underline me-4 md:me-6">Política de privacidade</Link>
           </li>
           <li>
-            <a href="#" className="hover:underline me-4 md:me-6">Trabalhe conosco</a>
+            <Link to="#" className="hover:underline me-4 md:me-6">Trabalhe conosco</Link>
           </li>
           <li>
             <a href="#" className="hover:underline">Contato</a>
