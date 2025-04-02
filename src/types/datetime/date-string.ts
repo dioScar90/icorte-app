e9fd0f3397d0e891b459fd8519b3e1d4034a9778
Diogo L Scarmagnani <diogols@live.com>
2025-04-02 09:08:08 -0300

    datetime custom types changed, unfortunatelly, because too heavy for typescript to think

--- src/types/datetime/date-string.ts
+++ src/types/datetime/date-string.ts
@@ -5,4 +5,5 @@ type YYYY = `20${ZeroToNine}${ZeroToNine}`
 type MM = `0${OneToNine}` | `1${0|1|2}`
 type DD = `0${OneToNine}` | `${1|2}${ZeroToNine}` | `3${0|1}`
 
-export type DateString = `${YYYY}-${MM}-${DD}`
+// export type DateString = `${YYYY}-${MM}-${DD}`
+export type DateString = `${number}-${number}-${number}`
